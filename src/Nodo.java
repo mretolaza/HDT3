fc81b1389e55fa5cae80fc7e2457795078944d02
ret16339 <ret16339@uvg.edu.gt>
2017-02-05 12:15:31 -0600

    ClaseNodoArreglodeerrores

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,2 +1,45 @@
 
 
+public class Nodo <T extends Comparable> implements Comparable{
+    protected T  Valor; 
+    protected Nodo Siguiente; 
+    
+    public Nodo(T valor)
+    {
+        this.Valor = valor;
+    }
+    
+    public void setValor(T valor)
+    {
+        this.Valor = valor;
+    }
+    
+    public T getValor()
+    {
+        return this.Valor;
+    } 
+    
+    public void setSiguiente(Nodo siguiente)
+    {
+        this.Siguiente = siguiente;
+    }
+    
+    public Nodo getSiguiente()
+    {
+        return this.Siguiente;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return Valor.toString();
+    }
+
+    @Override
+    public int compareTo(Object other) {
+        return Valor.compareTo(((Nodo) other).getValor()); 
+    }
+    
+    
+            
+}
